ee182586a120f908b136ceea49bebfdadfeb53dc
BuRy <124532675+2BuRy1@users.noreply.github.com>
2023-12-04 10:24:26 +0300

    Delete lab3/src directory
    Deleting...

--- lab3/src/Interfaces/ForEngine.java
+++
@@ -1,7 +0,0 @@
-package Interfaces;
-
-public interface ForEngine {
-    String testEngine();
-    String adjectives();
-}
-
--- lab3/src/Interfaces/Places.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-public interface Places {
-
-    String CurrentPlace();
-    String StartingPlace();
-}
-
