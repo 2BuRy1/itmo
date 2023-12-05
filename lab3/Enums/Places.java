113a724d45732b089f429530f8113126cb43b758
2BuRy1 <kot514877@gmail.com>
2023-12-05 12:34:51 +0300

    Лабораторная работа №3

---
+++ lab3/Enums/Places.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum Places {
+    CURRENTPLACE,
+    STARTINGPLACE
+
+}
---
+++ lab3/Interfaces/ForKarlson.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+import Entities.Malish;
+import Enums.Places;
+
+public interface ForKarlson {
+    void readyForAFlight();
+
+    void scream(Malish m);
+
+    void pressTheButton();
+
+    void increaseHeight(Malish malish);
+    void fly(Places places);
+
+}
